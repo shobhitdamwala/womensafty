ba628ea096cac6f13b78ced31ed0dd41a2ba4ca3
WomenSafty-ai <helpwomenacccc@gmail.com>
2025-04-27 15:17:32 +0530

    update5

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ app.use("/api/v1",admin)
 dbConnection();
 
 // Start the server
-app.listen(PORT,'0.0.0.0', () => {
+app.listen(PORT, () => {
     console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
